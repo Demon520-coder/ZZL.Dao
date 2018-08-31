a2bff9b8c43bbd85447cb8e5a49e88ecb7495d10
zzlProject <99105337@qq.com>
2018-08-31 18:58:43 +0800

    添加项目文件。

---
+++ ZZL.Common/StringExtension.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZZL.Common
+{
+    public static class StringExtension
+    {
+        public static bool IsNullOrEmpty(this string strSource)
+        {
+            return string.IsNullOrEmpty(strSource);
+        }
+
+        public static int TryToInt(this string strSource)
+        {
+            int defVal;
+
+            int.TryParse(strSource, out defVal);
+
+            return defVal;
+
+        }
+    }
+}
