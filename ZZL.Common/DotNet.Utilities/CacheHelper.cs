6d16a63a2c34b771ae9d8c057a7ac05a57a5917f
zzlProject <99105337@qq.com>
2018-09-01 17:57:37 +0800

    1.新增工具类;

---
+++ ZZL.Common/DotNet.Utilities/CacheHelper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace CZBK.Shop.Common
+{
+   public class CacheHelper
+    {
+       /// <summary>
+       /// 根据键从缓存中取出数据
+       /// </summary>
+       /// <param name="key"></param>
+       /// <returns></returns>
+       public static object Get(string key)
+       {
+           System.Web.Caching.Cache cache = HttpRuntime.Cache;
+           return cache[key];
+       }
+       public static void Insert(string key, object value)
+       {
+           System.Web.Caching.Cache cache = HttpRuntime.Cache;
+           cache.Insert(key, value);
+       }
+    }
+}
---
+++ ZZL.Common/DotNet.Utilities/页码条/MyPageBar.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CZBK.BookShop.Common
+{
+   public class MyPageBar
+    {
+       public static string CreatePageBar(int pageIndex, int pageCount)
+       {
+           if (pageCount == 1)
+           {
+               return string.Empty;
+           }
+           int start = pageIndex - 5;//计算起始位置.
+           start = start < 1 ? 1 : start;
+           int end = start + 9;//计算终止位置.
+           end = end > pageCount ? pageCount : end;
+           StringBuilder sb=new StringBuilder();
+           for (int i = start; i <= end;i++)
+           {
+               if (i == pageIndex)
+               {
+                   sb.Append(i);
+               }
+               else
+               {
+                   sb.Append(string.Format("<a href='?pageIndex={0}'>{0}</a>",i));
+               }
+           }
+           return sb.ToString();
+       }
+    }
+}
